eac1584ce21d7bfd6f4d5f957e6a179af0118351
Davide Mancusi <davide.mancusi@cea.fr>
2015-02-19 13:21:18 +0100

    Add a few docstrings

--- t4.py
+++ t4.py
@@ -1,8 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 import numpy as np
 
 class XMLResult:
+    """Extract data from the Tripoli-4® output file.
+
+    This class is responsible for extracting the calculation results, grids,
+    scores and responses from the Tripoli-4® XML output file.
+    """
 
     def __init__(self, fname):
+        """Initialize the instance from the XML file 'fname'.
+
+        Arguments:
+        fname -- the name of the XML file.
+        """
         from bs4 import BeautifulSoup
         with open(fname) as f:
             self.soup = BeautifulSoup(f.read(), 'lxml')
@@ -23,11 +36,17 @@ class XMLResult:
         return self.soup.list_decoupage.find('decoupage', recursive=False, attrs={'name': name})
 
     def grid(self, name):
+        """Return the specified grid as a numpy array.
+
+        Arguments:
+        name -- the name of the grid
+        """
         gridxml = self.grid_xml(name)
         grid = np.fromstring(gridxml.string, sep=' ', dtype=self.dtype)
         return grid
 
     def grid_names(self):
+        """Return the names of all defined grids, as a list."""
         return self._list_from_iterable_attr(self.xgrids_xml(), 'name')
 
     def xscores_xml(self):
@@ -38,6 +57,7 @@ class XMLResult:
         return self.soup.scores_definition.find_all('score', recursive=False)
 
     def score_names(self):
+        """Return the names of all defined scores, as a list."""
         return self._list_from_iterable_attr(self.xscores_xml(), 'name')
 
     def score_xml(self, name):
@@ -56,6 +76,7 @@ class XMLResult:
         return response
 
     def response_names(self):
+        """Return the names of all defined responses, as a list."""
         return self._list_from_iterable_attr(self.xresponses_xml(), 'name')
 
     def batch_results_xml(self, batch_num):
@@ -65,7 +86,17 @@ class XMLResult:
         else:
             raise ValueError("argument batch_num to XMLResult.batch_results_xml must be 'last' or a batch number (int)")
 
-    def batch_result(self, score_name, batch_num, normalize=True):
+    def batch_result(self, score_name, batch_num, divide_by_bin=True):
+        """Return the result for a given score in a given batch.
+
+        Arguments:
+        score_name -- name of the score (as a string)
+        batch_num -- the number of the batch.
+
+        Keyword arguments:
+        divide_by_bin -- whether the score result should be divided by the bin
+        size.
+        """
         if not isinstance(score_name,str):
             raise ValueError('argument score_name to XMLResult.batch_result must be a string')
         score = self.score(score_name)
@@ -79,7 +110,7 @@ class XMLResult:
         result = np.fromstring(resultxml.string, sep=' ', dtype=self.dtype)
         # divide by the bin width if requested
         width = np.ediff1d(grid)
-        if normalize:
+        if divide_by_bin:
             result /= width
         left = grid[:-1]
         return (left, result, width)
@@ -93,7 +124,19 @@ class XMLResult:
             raise ValueError("argument batch_num to XMLResult.mean_results_xml must be 'last' or a batch number (int)")
         return results
 
-    def mean_result(self, score_name, batch_num='last', normalize=True):
+    def mean_result(self, score_name, batch_num='last', divide_by_bin=True):
+        """Return the mean result for a given score.
+
+        Arguments:
+        score_name -- name of the score (as a string)
+
+        Keyword arguments:
+        batch_num -- results will be presented for the specified batch. Can be
+        an integer, in which case it is interpreted as a batch number, or
+        'last'.
+        divide_by_bin -- whether the score result should be divided by the bin
+        size.
+        """
         if not isinstance(score_name,str):
             raise ValueError('argument score_name to XMLResult.mean_result must be a string')
         score = self.score(score_name)
@@ -110,7 +153,7 @@ class XMLResult:
         sd = np.array(sd_list, dtype=self.dtype)
         # divide by the bin width if requested
         width = np.ediff1d(grid)
-        if normalize:
+        if divide_by_bin:
             val /= width
             sd /= width
         left = grid[:-1]
