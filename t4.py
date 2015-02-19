21d52ab3bd094fc1d4d77cee5b685fe6266cc5b3
Davide Mancusi <davide.mancusi@cea.fr>
2015-02-19 11:30:09 +0100

    Add mean_result method

--- t4.py
+++ t4.py
@@ -7,15 +7,17 @@ class XMLResult:
         with open(fname) as f:
             self.soup = BeautifulSoup(f.read(), 'lxml')
 
+        self.dtype = float
+
     def grids(self):
         for gridxml in self.soup.list_decoupage.find_all('decoupage', recursive=False):
-            grid = np.fromstring(gridxml.string, sep=' ')
+            grid = np.fromstring(gridxml.string, sep=' ', dtype=self.dtype)
             name = unicode(gridxml.get('name'))
             yield name, grid
 
     def grid(self, name):
         gridxml = self.soup.list_decoupage.find('decoupage', recursive=False, attrs={'name': name})
-        grid = np.fromstring(gridxml.string, sep=' ')
+        grid = np.fromstring(gridxml.string, sep=' ', dtype=self.dtype)
         return grid
 
     def scores(self):
@@ -44,20 +46,21 @@ class XMLResult:
     def batch_result(self, score_name, batch_num, normalize=True):
         if not isinstance(score_name,str):
             raise ValueError('argument score_name to XMLResult.batch_result must be a string')
-        results = self.batch_results(batch_num)
         score = self.score(score_name)
         if not score:
             raise ValueError('argument score_name to XMLResult.batch_result must be the name of a score')
         score_grid_name = score['nrj_dec']
         grid = self.grid(score_grid_name)
         score_id = score['id']
-        resultxml = results.find('result', scoreid=score_id)
-        result = np.fromstring(resultxml.gelement.string, sep=' ')
+        results = self.batch_results(batch_num)
+        resultxml = results.find('result', scoreid=score_id).gelement
+        result = np.fromstring(resultxml.string, sep=' ', dtype=self.dtype)
         # divide by the bin width if requested
+        width = np.ediff1d(grid)
         if normalize:
-            width = np.ediff1d(grid)
-            result = result/width
-        return grid, result
+            result /= width
+        left = grid[:-1]
+        return np.dstack((left, result, width))
 
     def mean_results(self, batch_num='last'):
         if batch_num=='last':
@@ -67,4 +70,27 @@ class XMLResult:
         else:
             raise ValueError("argument batch_num to XMLResult.mean_results must be 'last' or a batch number (int)")
         return results
+
+    def mean_result(self, score_name, normalize=True):
+        if not isinstance(score_name,str):
+            raise ValueError('argument score_name to XMLResult.mean_result must be a string')
+        score = self.score(score_name)
+        if not score:
+            raise ValueError('argument score_name to XMLResult.mean_result must be the name of a score')
+        score_grid_name = score['nrj_dec']
+        grid = self.grid(score_grid_name)
+        score_id = score['id']
+        results = self.mean_results()
+        resultxml = results.find('mean_result', scoreid=score_id).gelement
+        val_list = [ self.dtype(v.string) for v in resultxml.find_all('val') ]
+        sd_list = [ self.dtype(v.string) for v in resultxml.find_all('sd') ]
+        val = np.array(val_list, dtype=self.dtype)
+        sd = np.array(sd_list, dtype=self.dtype)
+        # divide by the bin width if requested
+        width = np.ediff1d(grid)
+        if normalize:
+            val /= width
+            sd /= width
+        left = grid[:-1]
+        return np.dstack((left, val, width, sd))
 
