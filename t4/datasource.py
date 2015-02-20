00b5f3eeb1ce06d300ca68b9eafc34af23ef1403
Davide Mancusi <davide.mancusi@cea.fr>
2015-02-20 18:39:01 +0100

    Add TXTResult class (stub)

--- t4/datasource.py
+++ t4/datasource.py
@@ -1,7 +1,6 @@
 # coding: utf-8
 
-import magic
-from xmlresult import XMLResult
+from xmlresult import XMLResult, TXTResult
 from collections import Mapping
 
 class SourceError(Exception):
@@ -9,7 +8,7 @@ class SourceError(Exception):
 
 class DataSource:
     def __init__(self, item):
-        self.default_options = {
+        self.default_xml_options = {
                 'batch_num': 'last',
                 'divide_by_bin': True,
                 }
@@ -36,7 +35,7 @@ class DataSource:
         if not isinstance(file_name, basestring) or not isinstance(score_name, basestring):
             raise SourceError
 
-        self.kwargs = self.default_options.copy()
+        self.kwargs = self.default_xml_options.copy()
 
         try:
             options = item[2]
@@ -46,22 +45,34 @@ class DataSource:
             raise SourceError
         self.kwargs.update(options)
 
-        with magic.Magic() as m:
-            magic_id = m.id_filename(file_name)
-
-            if 'XML' in magic_id:
-                xml_result = XMLResult(file_name)
-                self.result = xml_result.mean_result(
-                        score_name,
-                        batch_num=self.kwargs['batch_num'],
-                        divide_by_bin=self.kwargs['divide_by_bin']
-                        )
-            else:
-                raise Exception('for the moment we only accept XML input')
+        # Here we assume file_name is a valid, well-formed T4 output file
+        xml_result = XMLResult(file_name)
+        self.result = xml_result.mean_result(
+                score_name,
+                batch_num=self.kwargs['batch_num'],
+                divide_by_bin=self.kwargs['divide_by_bin']
+                )
 
         del self.kwargs['batch_num']
         del self.kwargs['divide_by_bin']
 
     def init_from_txt(self, item):
-        pass
+        """Initialize the data source from a text file.
+        """
 
+        try:
+            file_name = item[0]
+        except IndexError:
+            if not isinstance(iter, basestring):
+                raise SourceError
+            else:
+                file_name = item
+
+        try:
+            options = item[1]
+        except IndexError:
+            pass
+
+        # Here we assume file_name is a valid, well-formed T4 output file
+        xml_result = TXTResult(file_name)
+        self.result = xml_result.mean_result(**options)
