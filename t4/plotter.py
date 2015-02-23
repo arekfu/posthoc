b66dcc64db6fe12834c92e17965d7e311601f7a2
Davide Mancusi <davide.mancusi@cea.fr>
2015-02-23 11:21:28 +0100

    Fix: allow any PyPlot option on the command line

--- t4/plotter.py
+++ t4/plotter.py
@@ -15,15 +15,23 @@ class Plotter(object):
 
     def draw_step(self, data_source):
         result = data_source.result
-        kwargs = data_source.kwargs.copy()
-        step_artist, = self.axes.plot(result.edges, result.contents, drawstyle='steps-post', **kwargs)
+        step_args = data_source.kwargs.copy()
+        if 'marker' in step_args:
+            del step_args['marker']
+        if 'drawstyle' in step_args:
+            del step_args['drawstyle']
+        step_artist, = self.axes.plot(result.edges, result.contents, drawstyle='steps-post', marker=None, **step_args)
         centers = 0.5*(result.edges[1:]+result.edges[:-1])
-        if not 'color' in kwargs:
+
+        errorbar_args = data_source.kwargs.copy()
+        if not 'color' in errorbar_args:
             lc = plt.getp(step_artist, 'color')
-            kwargs['color'] = lc
+            errorbar_args['color'] = lc
+        if 'linestyle' in errorbar_args:
+            del errorbar_args['linestyle']
         try:
             yerr = result.errors[:-1]
         except TypeError:
             yerr = None
-        errorbar_artists = self.axes.errorbar(centers, result.contents[:-1], yerr=yerr, linestyle='none', **kwargs)
+        errorbar_artists = self.axes.errorbar(centers, result.contents[:-1], yerr=yerr, linestyle='none', **errorbar_args)
 
--- test_plot.py
+++ test_plot.py
@@ -10,7 +10,7 @@ directory = '/data/tmpdm2s/dm232107/calculations/t4/ttb/sphere/'
 photon_ds = XMLDataSource(directory + 'photon/photon.t4.xml', 'photon_spectrum', divide_by_bin=False)
 ttb_ds = XMLDataSource(directory + 'ttb/ttb.t4.xml', 'photon_spectrum', divide_by_bin=False)
 no_ttb_ds = XMLDataSource(directory + 'no_ttb/no_ttb.t4.xml', 'photon_spectrum', divide_by_bin=False)
-mcnp_ds = CSVDataSource(directory + 'mcnp/mcnp_bin_norm.dat', '0:1:2')
+mcnp_ds = CSVDataSource(directory + 'mcnp/mcnp_bin_norm.dat', '0:1:2', linestyle='none', marker='o')
 
 pm.energy_score([photon_ds, ttb_ds, no_ttb_ds, mcnp_ds])
 
