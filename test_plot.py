c3bf479fc61f476720c991fa95616ead9a8e8793
Davide Mancusi <davide.mancusi@cea.fr>
2015-02-24 13:18:37 +0100

    Add labels and other amenities to test case

--- test_plot.py
+++ test_plot.py
@@ -7,10 +7,10 @@ pm = PlotManager()
 
 directory = '/data/tmpdm2s/dm232107/calculations/t4/ttb/sphere/'
 
-photon_ds = XMLDataSource(directory + 'photon/photon.t4.xml', 'photon_spectrum', divide_by_bin=False)
-ttb_ds = XMLDataSource(directory + 'ttb/ttb.t4.xml', 'photon_spectrum', divide_by_bin=False)
-no_ttb_ds = XMLDataSource(directory + 'no_ttb/no_ttb.t4.xml', 'photon_spectrum', divide_by_bin=False)
-mcnp_ds = CSVDataSource(directory + 'mcnp/mcnp_bin_norm.dat', '0:1:2', linestyle='none', marker='o')
+photon_ds = XMLDataSource(directory + 'photon/photon.t4.xml', 'photon_spectrum', divide_by_bin=False, label='T4 photon')
+ttb_ds = XMLDataSource(directory + 'ttb/ttb.t4.xml', 'photon_spectrum', divide_by_bin=False, label='T4 TTB')
+no_ttb_ds = XMLDataSource(directory + 'no_ttb/no_ttb.t4.xml', 'photon_spectrum', divide_by_bin=False, label='T4 full')
+mcnp_ds = CSVDataSource(directory + 'mcnp/mcnp_bin_norm.dat', '0:1:2', linestyle='none', marker='o', label='MCNP')
 
 pm.draw([photon_ds, ttb_ds, no_ttb_ds, mcnp_ds])
 
