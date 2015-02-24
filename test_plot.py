29cd5d8734eb7a48c48ac65b9b488d3b37d98b79
Davide Mancusi <davide.mancusi@cea.fr>
2015-02-24 13:31:02 +0100

    test: improve indentation and specify directory as a CLI argument

--- test_plot.py
+++ test_plot.py
@@ -3,16 +3,46 @@
 from t4.plotmanager import PlotManager
 from t4.datasources import XMLDataSource, CSVDataSource
 import matplotlib.pyplot as plt
+import sys
+
+directory = sys.argv[1] if len(sys.argv)>2 else '/data/tmpdm2s/dm232107/calculations/t4/ttb/sphere/'
+
+photon = XMLDataSource(
+        directory + 'photon/photon.t4.xml',
+        'photon_spectrum',
+        divide_by_bin=False,
+        label='T4 photon',
+        marker='o'
+        )
+ttb = XMLDataSource(
+        directory + 'ttb/ttb.t4.xml',
+        'photon_spectrum',
+        divide_by_bin=False,
+        label='T4 TTB',
+        marker='*'
+        )
+no_ttb = XMLDataSource(
+        directory + 'no_ttb/no_ttb.t4.xml',
+        'photon_spectrum',
+        divide_by_bin=False,
+        label='T4 full',
+        marker='s'
+        )
+no_ttb_no_e = XMLDataSource(
+        directory + 'no_ttb_no_e/no_ttb_no_e.t4.xml',
+        'photon_spectrum',
+        divide_by_bin=False,
+        label='T4 no $e^+$/$e^-$',
+        marker='D'
+        )
+mcnp = CSVDataSource(
+        directory + 'mcnp/mcnp_bin_norm.dat',
+        '0:1:2',
+        label='MCNP',
+        marker='d'
+        )
 
 pm = PlotManager()
-
-directory = '/data/tmpdm2s/dm232107/calculations/t4/ttb/sphere/'
-
-photon_ds = XMLDataSource(directory + 'photon/photon.t4.xml', 'photon_spectrum', divide_by_bin=False, label='T4 photon')
-ttb_ds = XMLDataSource(directory + 'ttb/ttb.t4.xml', 'photon_spectrum', divide_by_bin=False, label='T4 TTB')
-no_ttb_ds = XMLDataSource(directory + 'no_ttb/no_ttb.t4.xml', 'photon_spectrum', divide_by_bin=False, label='T4 full')
-mcnp_ds = CSVDataSource(directory + 'mcnp/mcnp_bin_norm.dat', '0:1:2', linestyle='none', marker='o', label='MCNP')
-
-pm.draw([photon_ds, ttb_ds, no_ttb_ds, mcnp_ds])
+pm.draw([photon, ttb, no_ttb, no_ttb_no_e, mcnp])
 
 plt.show()
